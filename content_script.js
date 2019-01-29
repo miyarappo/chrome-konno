45903094df3d2af65fea3cebfcbaf19feb46de94
“Minoru” <miyarappo@gmail.com>
2019-01-29 16:50:14 +0900

    Fix version1.0

--- content_script.js
+++ content_script.js
@@ -1,46 +1,39 @@
-// var clickCount = 0
+var clickCount = 0
 
-// document.addEventListener('click', () => {
-//   if (clickCount > 1) {
-//     window.alert('まじでやっちまった')
-//     clickCount = 0
-//   } else {
-//     window.alert('やべっ、やべっ')
-//     clickCount++;
-//   }
-// })
-
-// document.addEventListener('load', (event) => {
-//   console.log(event)
-// })
-
-// const onMouse = evt => {
-  // console.log(evt)
-  // console.log('test', chrome.extension.getURL('icons/icon128.png'))
-  // const test = document.querySelector(evt.target)
-  // console.log(evt.target)
-  // evt.target.style.backgroundImage = 'https://www.dropbox.com/s/lj09xljtfp8c5zb/favicon.png?dl=0'
-  // console.log(evt.target)
-  // evt.target.style.border = "2px solid #f00"
-  // var icon = document.createElement('img').setAttribute('src', 'https://www.dropbox.com/s/lj09xljtfp8c5zb/favicon.png?dl=0')
-  // console.log(icon)
-  // evt.target.appendChild(icon)
-// }
-
-// document.addEventListener("click", onMouse)
-// document.removeEventListener("mousemove", onMouse)
+document.addEventListener('click', () => {
+  if (clickCount > 1) {
+    window.alert('まじでやっちまった')
+    clickCount = 0
+  } else {
+    window.alert('やべっ、やべっ')
+    clickCount++;
+  }
+})
 
 
-// const onMouse = evt => {
-//   evt.target.style.border = "2px solid #f00"
-//   _target = evt.target
-//   const selector = getSelectorFromElement(evt.target)
-//   chrome.runtime.sendMessage({selector: selector})
-// }
+// chrome.extension.getURL('icons/icon128.png') で取得出来なかったのでSNSから取得
+const images = [
+  'https://cdn.profile-image.st-hatena.com/users/y-konno0528/profile.png?1531320070',
+  'https://pbs.twimg.com/profile_images/1045808716799299589/G7BF-mgX_400x400.jpg'
+]
 
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   if (request.action == "activate") {
-//   } else if (request.action == "deactivate") {
-//     document.removeEventListener("mousemove", onMouse)
-//   }
-// })
+window.scrollY =  window.innerHeight
+
+var count = 1
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset > window.innerHeight*count) {
+    replaceImg()
+    count++
+  }
+})
+
+function replaceImg() {
+  const targetElements = document.querySelectorAll('img')
+
+  targetElements.forEach((element) => {
+    element.setAttribute('src', images[Math.floor(Math.random() * 2)])
+  })
+}
+
+replaceImg()
